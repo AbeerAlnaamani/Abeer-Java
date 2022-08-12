e6a7675b832123b58298f94fd118352c15a00179
AbeerAlnaamani <111145082+AbeerAlnaamani@users.noreply.github.com>
2022-08-12 21:12:55 +0300

    Add files via upload

---
+++ HW4.java
@@ -0,0 +1,10 @@
+public class HW4 {
+    public static void main(String[] args) {
+        System.out.println("Initial account balance is (1000)");
+    /* Estimated interest increase each year is (5%).
+       Balance at the end of the first year is (1000*1.05)=(1050).
+       Balance at the end of the second year is (1050*1.05)=(1102.5).
+       Balance at the end of the third year is (1102.5*1.05)=(1157.625). */
+        System.out.println("Account balance after three years is (1157.625)");
+    }
+}
---
+++ HW8.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+public class HW8 {
+    public static void main(String[] args) {
+            JOptionPane.showMessageDialog(null, "Hello, Abeer");
+        }
+    }
+
+
